9f046995b6b4559631cc4e8fea2666a59842a9c4
Anton <kozlovsky.anton@gmail.com>
2017-03-17 20:34:24 +0200

    redutant for only-dao

--- softgroup/basis/common-protocol/src/main/java/com/softgroup/common/protocol/RoutedAction.java
+++ softgroup/basis/common-protocol/src/main/java/com/softgroup/common/protocol/RoutedAction.java
@@ -5,15 +5,4 @@ import java.io.Serializable;
 public class RoutedAction<T extends Serializable> extends Action<T> {
 	private static final long serialVersionUID = -318012176965757271L;
 
-	private RoutedData routedData;
-
-
-	public RoutedData getRoutedData() {
-		return routedData;
-	}
-
-	public void setRoutedData(RoutedData routedData) {
-		this.routedData = routedData;
-	}
-
 }
--- softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/AbstractRequestHandler.java
+++ softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/AbstractRequestHandler.java
@@ -1,21 +1,12 @@
 package com.softgroup.common.router.api;
 
 
-import com.softgroup.common.datamapper.DataMapper;
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.RequestData;
 import com.softgroup.common.protocol.Response;
 import com.softgroup.common.protocol.ResponseData;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.lang.reflect.ParameterizedType;
-import java.util.Map;
-
-public abstract class AbstractRequestHandler<T extends RequestData, R extends ResponseData> implements RequestHandler {
-
-	@Autowired
-	private DataMapper dataMapper;
-
+public class AbstractRequestHandler<T extends RequestData, R extends ResponseData> implements RequestHandler {
 	@Override
 	public String getName() {
 		return null;
@@ -23,19 +14,7 @@ public abstract class AbstractRequestHandler<T extends RequestData, R extends Re
 
 	@Override
 	public Response<R> handle(Request<?> msg) {
-		Request<T> request = new Request<>();
-		request.setHeader(msg.getHeader());
-
-		final Class<T> cl = (Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[0];
-
-		T t = dataMapper.convert((Map<String, Object>) msg.getData(),cl);
-
-		request.setData(t);
-
-
-		return handleWork(request);
+		return null;
 	}
-
-	public abstract Response<R> handleWork(Request<T> msg);
 
 }
