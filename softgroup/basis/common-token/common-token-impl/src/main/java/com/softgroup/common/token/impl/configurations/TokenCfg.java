999c018738f80e1e860eb59ebcc62612215e0d6f
Anton <kozlovsky.anton@gmail.com>
2017-04-08 00:48:33 +0300

    remove not-rest module

--- softgroup/basis/common-token/common-token-impl/src/main/java/com/softgroup/common/token/impl/configurations/TokenCfg.java
+++
@@ -1,9 +0,0 @@
-package com.softgroup.common.token.impl.configurations;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.softgroup.common.token.impl")
-public class TokenCfg {
-}
--- softgroup/services/authorization/authorization-api/src/main/java/com/softgroup/authorization/api/router/AuthorizationRouterHandler.java
+++
@@ -1,8 +0,0 @@
-package com.softgroup.authorization.api.router;
-
-import com.softgroup.common.router.api.CommonRouterHandler;
-import com.softgroup.common.router.api.Handler;
-import com.softgroup.common.router.router.RouterHandler;
-
-public interface AuthorizationRouterHandler extends RouterHandler, CommonRouterHandler, Handler {
-}
