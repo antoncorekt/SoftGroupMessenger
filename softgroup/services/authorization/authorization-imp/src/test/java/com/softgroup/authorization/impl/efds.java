75604be753e396db0c1f8a127360afa1eaaa6f28
Anton <kozlovsky.anton@gmail.com>
2017-04-18 19:13:07 +0300

    add mocks test

---
+++ softgroup/services/authorization/authorization-imp/src/test/java/com/softgroup/authorization/impl/efds.java
@@ -0,0 +1,7 @@
+package com.softgroup.authorization.impl;
+
+/**
+ * Created by anton on 18.04.17.
+ */
+public class efds {
+}
---
+++ softgroup/services/authorization/authorization-imp/src/test/java/com/softgroup/authorization/impl/session/SessionDataTest.java
@@ -0,0 +1,7 @@
+package com.softgroup.authorization.impl.session;
+
+/**
+ * Created by anton on 18.04.17.
+ */
+public class SessionDataTest {
+}
